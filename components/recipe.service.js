80264b261aecddcd14c381d26581e2f4043bdc95
brameijm <brameijm@gmail.com>
2019-04-29 21:58:14 -0400

    added auth example in the recipe service file

--- components/recipe.service.js
+++ components/recipe.service.js
@@ -1,22 +1,96 @@
-function RecipeService($http) {
+function RecipeService($http, $q) {
     const service = this;
+ 
+    service.key = 'rEKWKvPrW7HpooXYxbjymSfIAOyKcwvvhixHIIWNAYlZeLLzmv'; // Fill in here
+    service._secret = 'OlpuSV27PqFDXswHBLBMPCHuTPh6DfxauvnwNSO5'; // Fill in here
+    service.token = '';
 
-     /**
-     * Call https://www.reddit.com/r/aww/.json
-     * and set ctrl.feed to be the results
-     */
-    service.fetchAwwServiceSubreddit = () =>{
-        // $http stuff goes here
-        // were we put the call to the api   
-        return $http({
-            method: "GET",
-            url: `https://www.reddit.com/r/aww/.json`
+    service.doAuth = () => {
+        return $q(function (resolve, reject) {
+            $http({
+                url: 'https://api.petfinder.com/v2/oauth2/token',
+                data: {
+                    grant_type: 'client_credentials',
+                    client_id: service.key,
+                    client_secret: service._secret
+                },
+                method: 'POST'
+            })
+                .then((response) => {
+                    service.token = response.data.access_token;
+                    resolve(service.token);
+                });
         })
+    };
 
+    service.getTypes = () => {
+        return $q(function (resolve, reject) {
+            service.doAuth()
+                .then((token) => {
+                    $http({
+                        url: 'https://api.petfinder.com/v2/types/',
+                        method: 'GET',
+                        headers: {
+                            'Authorization': 'Bearer ' + token
+                        }
+                    })
+                        .then((response) => {
+                            let types = [];
+
+                            // Go through each item and organize
+                            // the data into usable array
+                            response.data.types.forEach(function (type) {
+                                // See API docs for how this is organized
+                                types.push({
+                                    name: type.name,
+                                    value: type._links.self.href.split('/v2/types/')[1]
+                                })
+                            });
+
+                            console.log(types);
+                            resolve(types);
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                            reject(error);
+                        })
+
+                });
+        })
     }
 
-       
+    service.getData = (filter) => {
+        let url = 'https://api.petfinder.com/v2/animals';
+
+        // If a filter is supplied, add to URL to query by that type
+        if (filter) {
+            url += `?type=${filter}`;
+        }
+        return $q(function (resolve, reject) {
+            service.doAuth()
+                .then((token) => {
+                    $http({
+                        url: url,
+                        method: 'GET',
+                        headers: {
+                            'Authorization': 'Bearer ' + token
+                        }
+                    })
+                        .then((response) => {
+                            console.log(response)
+                            resolve(response.data.animals);
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                            reject(error);
+                        })
+                });
+        });
+    }
 }
+
+       
+
 
 angular
 .module('RecipeApp')
